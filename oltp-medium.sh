4141dbcb702fe92429c83653a0ec76aa208e364a
bombehub <liliangneu@126.com>
2018-09-26 19:49:34 +0800

    stable

---
+++ oltp-medium.sh
@@ -0,0 +1,6 @@
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t1 -x1
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t2 -x2
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t4 -x4
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t8 -x8
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t16 -x16
+./rundb -r0.9 -w0.1 -z0.8 -R16 -l8 -t32 -x32
